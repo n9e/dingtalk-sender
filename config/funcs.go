287fd8c8aaa7bb357ac97682811d6cf2cadb42d4
Cole_GG <Cole_GG@MacbookPro.local>
2020-05-28 15:57:03 +0800

    update 钉钉@人

--- config/funcs.go
+++ config/funcs.go
@@ -28,17 +28,18 @@ func InitLogger() {
 
 func Test(args []string) {
 	c := Get()
+	var mobile []string
 
 	dingClient := corp.New(c.DingTalk.Token, c.DingTalk.Mobiles,c.DingTalk.IsAtAll)
 
 	if len(args) == 0 {
-		fmt.Println("mail address not given")
+		fmt.Println("token not given")
 		os.Exit(1)
 	}
 
 	for i := 0; i < len(args); i++ {
 		token := args[i]
-		err := dingClient.Send(token, fmt.Sprintf("test message from n9e at %v", time.Now()))
+		err := dingClient.Send(token, mobile, fmt.Sprintf("test message from n9e at %v", time.Now()))
 		if err != nil {
 			logger.Error("test send to %s fail: %v\n", args[i], err)
 		} else {
